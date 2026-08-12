73b35de0bae0135c6ec77beb9c46ff07e65905b6
Nightsky <38932681+nightsky37@users.noreply.github.com>
2026-08-12 16:21:07 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        stk = []
+        for p, s in cars:
+            time = (target - p) / s
+            stk.append(time)
+
+            if len(stk) >= 2 and stk[-1] <= stk[-2]:
+                stk.pop()
+
+        return len(stk)
